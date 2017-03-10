492bcd023ffa5a3fc84331f69816cf66b3e91d65
xueleng <lengsnow2015@foxmail.com>
2017-03-10 15:35:01 +0800

    add bgpvpn, floatingip, gatewayconnection test

---
+++ scripts/tests/bgpvpn.py
@@ -0,0 +1,25 @@
+BGPVPNS = [
+    {
+        "bgpvpn": {
+            "status": "ACTIVE",
+            "type": "l3",
+            "name": "vpn1",
+            "admin_state_up": True,
+            "tenant_id": "9bacb3c5d39d41a79512987f338cf177",
+            "route_targets": "64512:1",
+            "networks": "3b80198d-4f7b-4f77-9ef5-774d54e17126",
+            "auto_aggregate": True,
+            "id": "4e8e5957-649f-477b-9e5b-f1f75b21c03c"
+        }
+    },
+    {
+        "bgpvpn": {
+            "status": "ACTIVE",
+            "name": "sample_bgpvpn_updated",
+            "admin_state_up": True,
+            "tenant_id": "4fd44f30292945e481c7b8a0c8908869",
+            "auto_aggregate": True,
+            "type": "l3"
+        }
+    }
+]
---
+++ scripts/tests/gatewayconnection.py
@@ -0,0 +1,21 @@
+GATEWAY_CONNECTIONS = [
+    {
+        "l2gateway_connection": {
+            "port_id": "9ea656c7-c9b8-4474-94f3-3b0bc741d9a9",
+            "gateway_id": "d3590f37-b072-4358-9719-71964d84a31c",
+            "segmentation_id": 100,
+            "network_id": "c69933c1-b472-44f9-8226-30dc4ffd454c",
+            "id": "3b0ef8f4-82c7-44d4-a4fb-6177f9a21977",
+            "tenant_id": "45977fa2dbd7482098dd68d0d8970117"
+        }
+    },
+    {
+        "l2gateway_connection": {
+            "gateway_id": "5227c228-6bba-4bbe-bdb8-6942768ff0f1",
+            "segmentation_id": 100,
+            "network_id": "9227c228-6bba-4bbe-bdb8-6942768ff0f1",
+            "id": "5227c228-6bba-4bbe-bdb8-6942768ff0e1",
+            "tenant_id": "de0a7495-05c4-4be0-b796-1412835c6820"
+        }
+    }
+]
