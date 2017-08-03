023566279db4c566ce40c4bbb51fa3c2987f7bcc
mikewell <mikewell@163.com>
2017-08-03 17:15:07 +0800

    Main Test with round test feature

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,7 @@
 import logging
 import latency
+import time
+import os
 
 from scripts import utils
 from scripts.tests.network import Network
@@ -30,32 +32,45 @@ from scripts.tests.sfcportpair import SFCPortPair
 from scripts.tests.sfcportpairgroup import SFCPortPairGroup
 
 if __name__ == '__main__':
+
     logging_config = utils.get_logging_config('logging')
-    logging.basicConfig(filename=logging_config['filename'], level=logging_config['level'])
-    Network.perform_tests('server', 'admin')
-    Router.perform_tests('server', 'Lily')
-    Subnet.perform_tests('server', 'Tom')
-    Port.perform_tests('server', 'Tom')
-    Trunk.perform_tests('server', 'Gary')
-    Bgpvpn.perform_tests('server', 'Gary')
-    Firewall.perform_tests('server', 'Tom')
-    FirewallPolicy.perform_tests('server', 'Tom')
-    FirewallRule.perform_tests('server', 'Tom')
-    FloatingIP.perform_tests('server', 'Gary')
-    Gateway.perform_tests('server', 'admin')
-    GatewayConnection.perform_tests('server', 'admin')
-    Loadbalancer.perform_tests('server', 'Gary')
-    LoadbalancerHealthMonitor.perform_tests('server', 'Tom')
-    LoadbalancerListener.perform_tests('server', 'Jack')
-    LoadbalancerPool.perform_tests('server', 'admin')
-    MeteringLabel.perform_tests('server', 'Gary')
-    MeteringLabelRule.perform_tests('server', 'Tom')
-    QosPolicy.perform_tests('server', 'Tom')
-    VpnService.perform_tests('server', 'admin')
-    # SecurityGroup.perform_tests('server', 'admin')
-    SecurityGroupRule.perform_tests('server', 'Gary')
-    SFCFlowClassifier.perform_tests('server', 'admin')
-    SFCPortChain.perform_tests('server', 'admin')
-    SFCPortPair.perform_tests('server', 'admin')
-    SFCPortPairGroup.perform_tests('server', 'admin')
-    latency.latency_data_transform()
+    filename = logging_config['filename']
+
+    if os.path.exists(filename):
+        os.remove(filename)
+
+    logging.basicConfig(filename=filename, level=logging_config['level'])
+
+    for i in range(2):
+        print('Test Round ' + str(i) + ' ' + time.strftime('%Y-%m-%d', time.localtime(time.time())))
+        logging.info('Test Round ' + str(i) + ' ' + time.strftime('%Y-%m-%d', time.localtime(time.time())))
+
+        Network.perform_tests('server', 'admin')
+        Router.perform_tests('server', 'Lily')
+        Subnet.perform_tests('server', 'Tom')
+        Port.perform_tests('server', 'Tom')
+        Trunk.perform_tests('server', 'Gary')
+        Bgpvpn.perform_tests('server', 'Gary')
+        Firewall.perform_tests('server', 'Tom')
+        FirewallPolicy.perform_tests('server', 'Tom')
+        FirewallRule.perform_tests('server', 'Tom')
+        FloatingIP.perform_tests('server', 'Gary')
+        Gateway.perform_tests('server', 'admin')
+        GatewayConnection.perform_tests('server', 'admin')
+        Loadbalancer.perform_tests('server', 'Gary')
+        LoadbalancerHealthMonitor.perform_tests('server', 'Tom')
+        LoadbalancerListener.perform_tests('server', 'Jack')
+        LoadbalancerPool.perform_tests('server', 'admin')
+        MeteringLabel.perform_tests('server', 'Gary')
+        MeteringLabelRule.perform_tests('server', 'Tom')
+        QosPolicy.perform_tests('server', 'Tom')
+        VpnService.perform_tests('server', 'admin')
+        # SecurityGroup.perform_tests('server', 'admin')
+        SecurityGroupRule.perform_tests('server', 'Gary')
+        SFCFlowClassifier.perform_tests('server', 'admin')
+        SFCPortChain.perform_tests('server', 'admin')
+        SFCPortPair.perform_tests('server', 'admin')
+        SFCPortPairGroup.perform_tests('server', 'admin')
+        latency.latency_data_transform(filename)
+
+
